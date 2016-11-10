5e099483fafc609db60ac3b460547b26b01da8f6
Sondre Groneng <songro93@gmail.com>
2016-11-10 22:46:51 +0100

    testing

--- wuo-1.php
+++ wuo-1.php
@@ -12,11 +12,11 @@
 
 <body>
 </body>
+<div class="page-header">
+    <h1>Assignment 1 <small>Introduction to web development</small></h1>
+</div>
 <?php include("./resources/includes/navbar.html") ?>
 <div class="container">
-    <div class="page-header">
-        <h1>Assignment 1 <small>Introduction to web development</small></h1>
-    </div>
     <div class="embed-responsive embed-responsive-16by9">
         <iframe class="embed-responsive-item" src="https://sites.google.com/site/wuoving1sondregroneng/">
         </iframe>
