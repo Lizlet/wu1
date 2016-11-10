b3be217b9440bf82e218aaaa1feca0ac2492fe88
Sondre Groneng <songro93@gmail.com>
2016-11-10 22:37:51 +0100

    setting up A1

---
+++ wuo-1.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Web Development - Year 1</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+</body>
+<?php include("./resources/includes/navbar.html") ?>
+<div class="container">
+    <div class="page-header">
+        <h1>Assignment 1 <small>Introduction to web development</small></h1>
+    </div>
+    <div class="embed-responsive embed-responsive-16by9">
+        <iframe class="embed-responsive-item" src="https://sites.google.com/site/wuoving1sondregroneng/">
+        </iframe>
+    </div>
+</div>
+
+</html>
