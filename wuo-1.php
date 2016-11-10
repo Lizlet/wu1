9a5d1aee548b6762e2e3784c4f04e41c1252d4f0
Sondre Groneng <songro93@gmail.com>
2016-11-10 23:14:28 +0100

    moved common head data to separate  file

--- wuo-1.php
+++ wuo-1.php
@@ -3,12 +3,7 @@
 
 <head>
     <title>Assignment 1 - Introduction to web development</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="./resources/stylesheets/sticky-footer-stylesheet.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <?php include("./resources/includes/head.html") ?>
 </head>
 
 <body>
