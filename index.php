b87176eace6a6c5cbc18787005d9bf7c4633e99b
Sondre Groneng <songro93@gmail.com>
2016-11-10 23:17:30 +0100

    redid index to use php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Web Development - Year 1</title>
+    <?php include("./resources/includes/head.html") ?>
+</head>
+
+<body>
+</body>
+<?php include("./resources/includes/navbar.html") ?>
+<div class="container">
+    <div class="page-header">
+        <h1>Home <small>Web development assignments for Sondre Grøneng</small></h1>
+    </div>
+</div>
+<?php include("./resources/includes/footer.html") ?>
+</html>
