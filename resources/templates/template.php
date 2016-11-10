7236783c3309f921fc7389b256b7bfa7855c32c0
Sondre Groneng <songro93@gmail.com>
2016-11-10 23:30:18 +0100

    created a template .php file and started on wuo-2

---
+++ resources/templates/template.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Placeholder</title>
+    <?php include("./resources/includes/head.html") ?>
+</head>
+
+<body>
+</body>
+
+<?php include("./resources/includes/navbar.html") ?>
+
+<div class="container">
+    <div class="page-header">
+        <h1>Placeholder <small>Placeholder</small></h1>
+    </div>
+</div>
+
+<?php include("./resources/includes/footer.html") ?>
+
+</html>
---
+++ wuo-2.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Assignment 2 - Simple HTML</title>
+    <?php include("./resources/includes/head.html") ?>
+</head>
+
+<body>
+</body>
+
+<?php include("./resources/includes/navbar.html") ?>
+
+<div class="container">
+    <div class="page-header">
+        <h1>Assignment 2 <small>Simple HTML</small></h1>
+    </div>
+</div>
+
+<?php include("./resources/includes/footer.html") ?>
+
+</html>
