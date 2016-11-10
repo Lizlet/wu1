06727bf18258746eaf8f8578a6828593df0b3c03
Sondre Groneng <songro93@gmail.com>
2016-11-10 23:46:09 +0100

    lorem ipsum for wuo-2

--- wuo-2.php
+++ wuo-2.php
@@ -15,6 +15,33 @@
     <div class="page-header">
         <h1><strong>Assignment 2</strong> <small>Simple HTML</small></h1>
     </div>
+
+    <h2>Lorem ipsum dolor sit amet, consectetur</h2>
+    <p>
+        Adipiscing elit. Quae autem natura suae primae institutionis oblita est? Ergo adhuc, quantum equidem intellego, causa non videtur fuisse mutandi nominis. Portenta haec esse dicit, neque ea ratione ullo modo posse vivi; Duo Reges: constructio interrete. Critolaus imitari voluit antiquos, et quidem est gravitate proximus, et redundat oratio, ac tamen is quidem in patriis institutis manet. Nunc de hominis summo bono quaeritur; Sed quoniam et advesperascit et mihi ad villam revertendum est, nunc quidem hactenus; Minime vero probatur huic disciplinae, de qua loquor, aut iustitiam aut amicitiam propter utilitates adscisci aut probari. Ita finis bonorum existit secundum naturam vivere sic affectum, ut optime is affici possit ad naturamque accommodatissime. Egone quaeris, inquit, quid sentiam? Id enim natura desiderat. Tollit definitiones, nihil de dividendo ac partiendo docet, non quo modo efficiatur concludaturque ratio tradit, non qua via captiosa solvantur ambigua distinguantur ostendit;
+    </p>
+
+    <h2>Nemo nostrum istius generis</h2>
+    <p>
+        Asotos iucunde putat vivere. Sed quanta sit alias, nunc tantum possitne esse tanta. Nec tamen ullo modo summum pecudis bonum et hominis idem mihi videri potest. Nam et complectitur verbis, quod vult, et dicit plane, quod intellegam; Erit enim instructus ad mortem contemnendam, ad exilium, ad ipsum etiam dolorem. Quam tu ponis in verbis, ego positam in re putabam. Stuprata per vim Lucretia a regis filio testata civis se ipsa interemit. Quam ob rem tandem, inquit, non satisfacit? His similes sunt omnes, qui virtuti student levantur vitiis, levantur erroribus, nisi forte censes Ti.
+    </p>
+
+    <h2>Quae possunt eadem contra Carneadeum</h2>
+    <p>
+        Illud summum bonum dici, quod is non tam, ut probaret, protulit, quam ut Stoicis, quibuscum bellum gerebat, opponeret. Miserum hominem! Si dolor summum malum est, dici aliter non potest. Sed erat aequius Triarium aliquid de dissensione nostra iudicare. Intrandum est igitur in rerum naturam et penitus quid ea postulet pervidendum; Tum Torquatus: Prorsus, inquit, assentior; Mihi, inquam, qui te id ipsum rogavi? His singulis copiose responderi solet, sed quae perspicua sunt longa esse non debent. Equidem etiam Epicurum, in physicis quidem, Democriteum puto. Est tamen ea secundum naturam multoque nos ad se expetendam magis hortatur quam superiora omnia.
+    </p>
+
+    <h2>Magno hic ingenio, sed res se tamen sic habet</h2>
+    <p>
+        Ut nimis imperiosi philosophi sit vetare meminisse. Quis est autem dignus nomine hominis, qui unum diem totum velit esse in genere isto voluptatis? Non enim ipsa genuit hominem, sed accepit a natura inchoatum. Ita similis erit ei finis boni, atque antea fuerat, neque idem tamen; In motu et in statu corporis nihil inest, quod animadvertendum esse ipsa natura iudicet? Hos contra singulos dici est melius. Sed id ne cogitari quidem potest quale sit, ut non repugnet ipsum sibi.
+    </p>
+
+    <h2>Si quae forte-possumus</h2>
+    <p>
+        Sed haec ab Antiocho, familiari nostro, dicuntur multo melius et fortius, quam a Stasea dicebantur. Ex quo intellegitur, quoniam se ipsi omnes natura diligant, tam insipientem quam sapientem sumpturum, quae secundum naturam sint, reiecturumque contraria. Num igitur dubium est, quin, si in re ipsa nihil peccatur a superioribus, verbis illi commodius utantur? Itaque his sapiens semper vacabit. Quod enim vituperabile est per se ipsum, id eo ipso vitium nominatum puto, vel etiam a vitio dictum vituperari. Ne vitationem quidem doloris ipsam per se quisquam in rebus expetendis putavit, nisi etiam evitare posset. Quando enim Socrates, qui parens philosophiae iure dici potest, quicquam tale fecit? Non quaero, quid dicat, sed quid convenienter possit rationi et sententiae suae dicere. Iam quae corporis sunt, ea nec auctoritatem cum animi partibus, comparandam et cognitionem habent faciliorem.
+    </p>
+
+
 </div>
 
 <?php include("./resources/includes/footer.html") ?>
