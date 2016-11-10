569cfb2f131a3466898e67b2d4315d1035085cf6
Sondre Groneng <songro93@gmail.com>
2016-11-10 22:57:15 +0100

    dont mind

--- wuo-1.php
+++ wuo-1.php
@@ -11,6 +11,8 @@
 </head>
 
 <body>
+
+</body>
 <?php include("./resources/includes/navbar.html") ?>
 <div class="container">
     <header class="page-header">
@@ -22,5 +24,4 @@
     </div>
 </div>
 <?php include("./resources/includes/footer.html") ?>
-</body>
 </html>
