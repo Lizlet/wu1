c77d75a7df158cd86e0e4313582c8b0cb91df999
Sondre Groneng <songro93@gmail.com>
2016-11-10 16:29:20 +0100

    embedded youtube video and own video.

---
+++ wuo-8/resources/wuo-8.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $("#YV").click(function() {
+        $("#the-frame").attr('src', "https://www.youtube.com/embed/7U-RbOKanYs");
+    });
+
+    $("#OV").click(function() {
+        $("#the-frame").attr('src', "resources/044594000-view-midway-island.mp4")
+    });
+});
